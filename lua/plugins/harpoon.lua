161f60cb4d8976dcdea5dae78e5222638cfd4ef4
bkamm <brycekamm@gmail.com>
2025-09-01 22:30:58 -0600

    Added harpoon and switched keybindings

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,55 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+
+    -- REQUIRED
+    harpoon:setup()
+    -- REQUIRED
+
+    vim.keymap.set('n', '<leader>a', function()
+      harpoon:list():add()
+    end, { desc = 'Harpoon: [A]dd file' })
+    vim.keymap.set('n', '<leader>h', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, { desc = 'Harpoon: Toggle menu' })
+
+    vim.keymap.set('n', '<leader>1', function()
+      harpoon:list():select(1)
+    end, { desc = 'Harpoon: Go to file 1' })
+    vim.keymap.set('n', '<leader>2', function()
+      harpoon:list():select(2)
+    end, { desc = 'Harpoon: Go to file 2' })
+    vim.keymap.set('n', '<leader>3', function()
+      harpoon:list():select(3)
+    end, { desc = 'Harpoon: Go to file 3' })
+    vim.keymap.set('n', '<leader>4', function()
+      harpoon:list():select(4)
+    end, { desc = 'Harpoon: Go to file 4' })
+    vim.keymap.set('n', '<leader>5', function()
+      harpoon:list():select(5)
+    end, { desc = 'Harpoon: Go to file 5' })
+    vim.keymap.set('n', '<leader>6', function()
+      harpoon:list():select(6)
+    end, { desc = 'Harpoon: Go to file 6' })
+    vim.keymap.set('n', '<leader>7', function()
+      harpoon:list():select(7)
+    end, { desc = 'Harpoon: Go to file 7' })
+    vim.keymap.set('n', '<leader>8', function()
+      harpoon:list():select(8)
+    end, { desc = 'Harpoon: Go to file 8' })
+    vim.keymap.set('n', '<leader>9', function()
+      harpoon:list():select(9)
+    end, { desc = 'Harpoon: Go to file 9' })
+
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set('n', '<leader>p', function()
+      harpoon:list():prev()
+    end, { desc = 'Harpoon: [P]revious file' })
+    vim.keymap.set('n', '<leader>n', function()
+      harpoon:list():next()
+    end, { desc = 'Harpoon: [N]ext file' })
+  end,
+}
