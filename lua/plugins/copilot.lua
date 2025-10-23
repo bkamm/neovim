0e9e3ee09dc2c4b91bdf872764654ee5badf12dc
bkamm <brycekamm@gmail.com>
2025-10-23 15:22:00 -0700

    Change copilot accept to <C-J> instead of tab

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,14 @@
 return {
   'github/copilot.vim',
+  config = function()
+    -- Disable Copilot’s default <Tab> mapping
+    vim.g.copilot_no_tab_map = true
+
+    -- Map <C-J> to accept the current suggestion
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+      desc = 'Accept Copilot suggestion',
+    })
+  end,
 }
